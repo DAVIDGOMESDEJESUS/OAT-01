5a31822fcdd8860430eb1c54cf8c43ec6bf0f7dc
Davidgomes6438 <101217381+DAVIDGOMESDEJESUS@users.noreply.github.com>
2023-11-02 18:09:40 -0300

    Add files via upload

---
+++ OAT-1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace POO1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2) Solicitar o tempo de casada para mulheres casadas
+            Console.Write("Digite o nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o sexo (M/F): ");
+            char sexo = char.Parse(Console.ReadLine());
+            Console.Write("Digite o estado civil: ");
+            string estadoCivil = Console.ReadLine();
+
+            if (sexo == 'F' && estadoCivil == "CASADA")
+            {
+                Console.Write("Digite o tempo de casada (anos): ");
+                int tempoCasada = int.Parse(Console.ReadLine());
+                Console.WriteLine("Tempo de casada (anos): " + tempoCasada);
+            }
+        }
+    }
+}
+
